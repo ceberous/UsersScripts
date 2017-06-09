e2ba5380593e2c96f910ed9d45f1a069c09a7d3e
ceberous <cerbus.collin@gmail.com>
2017-06-09 02:42:02 -0400

    Create LichessColorizeCoordinates.js

--- LichessColorizeCoordinates.js
+++ LichessColorizeCoordinates.js
@@ -1,47 +1,109 @@
 // ==UserScript==
-// @name          liChess.org Colorize Coordinates
+// @name          liChess.org Colorize Arrows
 // @namespace     http://userstyles.org
-// @description	  lichesscoordinates
+// @description	  lichessarrows
 // @author        ceberous
 // @homepage      https://creatitees.info
-// @include       https://en.lichess.org/*
+// @include       https://lichess.org/*
+// @require		  http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @run-at        document-start
-// @version       0.6
+// @version       0.1
 // ==/UserScript==
 
-(function() {var css = [
-	
+( function() {
 
-	"@import url(http://fonts.googleapis.com/css?family=Armata);",
+var colorPreferences = {};
+colorPreferences.colors = {
+    green: "#009933",
+    yellow: "#ccff33",
+    purple: "#cc00cc",
+    orange: "#ff3300",
+    red: "#ff0000",
+    blue: "#0066cc",
+};
+colorPreferences.arrows = {
+    large: { color: colorPreferences.colors.green , opacity: "1.0" },
+    medium: { color: colorPreferences.colors.yellow , opacity: "0.8" },
+    small: { color: colorPreferences.colors.blue , opacity: "0.8" },
+    xs: { color: colorPreferences.colors.purple , opacity: "0.6" },
+    xxs: { color: colorPreferences.colors.red , opacity: "0.5" },
+};
 
-	"@import url(http://fonts.googleapis.com/css?family=Atomic+Age|Sigmar+One|Fontdiner+Swanky|Graduate|Luckiest+Guy|Freckle+Face|Iceland|UnifrakturMaguntia|Titan+One|Oregano|Miltonian+Tattoo|Raleway+Dots|Milonga|Pirata+One|Wellfleet|Medula+One);",
+var clickWatchInterval = null;
+function startInterval() {
 
-	" .ranks {text-shadow: -2px -2px 0 #FF6F00, -2px -1px 0 #FF6F00, -2px 0px 0 #FF6F00, -2px 1px 0 #FF6F00, -2px 2px 0 #FF6F00, -1px -2px 0 #FF6F00, -1px -1px 0 #FF6F00, -1px 0px 0 #FF6F00, -1px 1px 0 #FF6F00, -1px 2px 0 #FF6F00, 0px -2px 0 #FF6F00, 0px -1px 0 #FF6F00, 0px 0px 0 #FF6F00, 0px 1px 0 #FF6F00, 0px 2px 0 #FF6F00, 1px -2px 0 #FF6F00, 1px -1px 0 #FF6F00, 1px 0px 0 #FF6F00, 1px 1px 0 #FF6F00, 1px 2px 0 #FF6F00, 2px -2px 0 #FF6F00, 2px -1px 0 #FF6F00, 2px 0px 0 #FF6F00, 2px 1px 0 #FF6F00, 2px 2px 0 #FF6F00; color: #000000;}",
-
-	" .files {text-shadow: -2px -2px 0 #FF6F00, -2px -1px 0 #FF6F00, -2px 0px 0 #FF6F00, -2px 1px 0 #FF6F00, -2px 2px 0 #FF6F00, -1px -2px 0 #FF6F00, -1px -1px 0 #FF6F00, -1px 0px 0 #FF6F00, -1px 1px 0 #FF6F00, -1px 2px 0 #FF6F00, 0px -2px 0 #FF6F00, 0px -1px 0 #FF6F00, 0px 0px 0 #FF6F00, 0px 1px 0 #FF6F00, 0px 2px 0 #FF6F00, 1px -2px 0 #FF6F00, 1px -1px 0 #FF6F00, 1px 0px 0 #FF6F00, 1px 1px 0 #FF6F00, 1px 2px 0 #FF6F00, 2px -2px 0 #FF6F00, 2px -1px 0 #FF6F00, 2px 0px 0 #FF6F00, 2px 1px 0 #FF6F00, 2px 2px 0 #FF6F00; color: #000000;}",
-
-	".move-dest { background: radial-gradient( transparent 0% ,transparent 80% , rgba( 229 , 126 , 0 , 1 ) 80% ) !important; } ",
-
-	"",
-	
-	
-
-].join("\n");
-if (typeof GM_addStyle != "undefined") {
-	GM_addStyle(css);
-} else if (typeof PRO_addStyle != "undefined") {
-	PRO_addStyle(css);
-} else if (typeof addStyle != "undefined") {
-	addStyle(css);
-} else {
-	var node = document.createElement("style");
-	node.type = "text/css";
-	node.appendChild(document.createTextNode(css));
-	var heads = document.getElementsByTagName("head");
-	if (heads.length > 0) {
-		heads[0].appendChild(node); 
-	} else {
-		document.documentElement.appendChild(node);
-	}
+    clickWatchInterval = setInterval( overDrawArrows , 250 );
 }
+
+
+function clearInterval() {
+    clearInterval(clickWatchInterval);
+}
+
+function overDrawArrows() {
+
+    $("[marker-end]").each( function( i , ielem ) {
+
+        var wSW = parseInt( $( ielem ).attr( "stroke-width" ) );
+        switch( wSW ) {
+            case 15:
+                $( ielem ).attr( "stroke" , colorPreferences.arrows.large.color );
+                $( ielem ).attr( "opacity" , colorPreferences.arrows.large.opacity );
+                break;
+            case 12:
+                $( ielem ).attr( "stroke" , colorPreferences.arrows.medium.color );
+                $( ielem ).attr( "opacity" , colorPreferences.arrows.medium.opacity );
+                break;
+            case 11:
+                $( ielem ).attr( "stroke" , colorPreferences.arrows.small.color );
+                $( ielem ).attr( "opacity" , colorPreferences.arrows.small.opacity );
+                break;
+            case 10:
+                $( ielem ).attr( "stroke" , colorPreferences.arrows.small.color );
+                $( ielem ).attr( "opacity" , colorPreferences.arrows.small.opacity );
+                break;
+            case 8:
+                $( ielem ).attr( "stroke" , colorPreferences.arrows.xs.color );
+                $( ielem ).attr( "opacity" , colorPreferences.arrows.xs.opacity );
+                break;
+            /*     
+            case 6:
+                $( ielem ).attr( "stroke" , colorPreferences.arrows.xs.color );
+                $( ielem ).attr( "opacity" , colorPreferences.arrows.xs.opacity );
+                break;                 
+            case 2:
+                $( ielem ).attr( "stroke" , colorPreferences.arrows.xs.color );
+                $( ielem ).attr( "opacity" , colorPreferences.arrows.xs.opacity );
+                break;
+            */
+            default:
+                $( ielem ).attr( "stroke" , colorPreferences.arrows.xxs.color );
+                $( ielem ).attr( "opacity" , colorPreferences.arrows.xxs.opacity );
+                break;
+        }
+
+    });
+}
+
+
+$(document).ready(function(){
+
+    startInterval();
+    /*
+    $(document).click(function(e) {
+        //alert("left click");
+        //if ( clickWatchInterval === null ) { startInterval(); }
+        overDrawArrows();
+    });
+
+    $(document).bind("contextmenu",function(e){
+        e.preventDefault();
+        //alert("right click");
+        //if ( clickWatchInterval === null ) { startInterval(); }
+        overDrawArrows();
+    });
+    */
+
+});
+
 })();
